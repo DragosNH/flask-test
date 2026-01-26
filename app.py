f498966866a65ecda2475846388d9d28118d2c00
Dragoş <edragos33@gmail.com>
2026-01-26 16:04:14 +0100

    project started and kicking

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/my-page")
+def my_page():
+    return render_template('mypage.html')
+
+@app.route("/my-fans")
+def my_fans():
+    return render_template('myfans.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
