031c04c3d777ec9c87983476b9a1b8cb0f141a77
Dragoş <edragos33@gmail.com>
2026-01-30 08:08:22 +0100

    added flask login manager

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 import os
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login_manager = LoginManager()
+
 
 @app.route("/")
 def index():
@@ -18,6 +21,10 @@ def my_fans():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
