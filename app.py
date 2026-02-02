23337fd0eb2a5da4dd7b4f28a60ef4b12965fc0e
Dragoş <edragos33@gmail.com>
2026-02-02 06:00:46 +0100

    signup page added

--- app.py
+++ app.py
@@ -52,7 +52,9 @@ def login():
     return render_template('/index.html')
     
 
-
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
